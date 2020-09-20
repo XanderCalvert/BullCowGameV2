2de633132ff9604496141ef5ec0cb133d02bc2ba
Matt Calvert <xander.raymatt@gmail.com>
2020-09-20 23:43:34 +0100

    abstraction and encapsulation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
+#include <iostream>
+#include <string>
+#include "main.h"
 
+void PrintIntro();
+std::string GetGuessAndPrintBack();
 
+// entry point for app
 int main() {
+
+	PrintIntro();
+	GetGuessAndPrintBack();
+
 	return 0;
+}
+
+void PrintIntro() {
+	// introduce the game
+	constexpr int WORD_LENGTH = 6;
+	std::cout << "Wellcome to Bulls and Cows, a fun word game" << std::endl;
+	std::cout << "Can you guess the " << WORD_LENGTH << " letter isogram I am thinking of?" << std::endl << std::endl;
+	return;
+}
+
+std::string GetGuessAndPrintBack() {
+	// get guess from the player
+	std::string Guess = "";
+	std::cout << "What is your guess? ";
+	std::getline(std::cin, Guess);
+
+	// repeat the guess back to them
+	std::cout << "You guessed the word: " << Guess << std::endl;
+	return Guess;
 }
