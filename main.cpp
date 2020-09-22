c8075f36fa65cedd98306b8ebaf1e94e5718449b
Matt Calvert <xander.raymatt@gmail.com>
2020-09-22 17:13:03 +0100

    refactoring

--- main.cpp
+++ main.cpp
@@ -3,37 +3,42 @@
 #include "main.h"
 
 void PrintIntro();
-std::string GetGuessAndPrintBack();
+void PlayGame();
+std::string GetGuess();
 
 // entry point for app
 int main() {
 
 	PrintIntro();
 
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int count = 0; count <= NUMBER_OF_TURNS; count++) {
-		GetGuessAndPrintBack();
-		std::cout << std::endl;
-	}
+	// loop for the number of turns asking for guesses
+	PlayGame();
 
-	return 0;
+	return 0; // exit application
 }
 
 void PrintIntro() {
 	// introduce the game
-	constexpr int WORD_LENGTH = 6;
+	constexpr int WORD_LENGTH = 5;
 	std::cout << "Wellcome to Bulls and Cows, a fun word game" << std::endl;
 	std::cout << "Can you guess the " << WORD_LENGTH << " letter isogram I am thinking of?" << std::endl << std::endl;
 	return;
 }
 
-std::string GetGuessAndPrintBack() {
+void PlayGame() {
+	constexpr int NUMBER_OF_TURNS = 4;
+	for (int count = 0; count <= NUMBER_OF_TURNS; count++) {
+		std::string Guess = GetGuess();
+		std::cout << "You guessed the word: " << Guess << std::endl << std::endl;
+	}
+};
+
+
+
+std::string GetGuess() {
 	// get guess from the player
 	std::string Guess = "";
 	std::cout << "What is your guess? ";
 	std::getline(std::cin, Guess);
-
-	// repeat the guess back to them
-	std::cout << "You guessed the word: " << Guess << std::endl;
 	return Guess;
 }
